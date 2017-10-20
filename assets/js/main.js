f0fac144773600a3ff4e7c400d498e60dfdded5b
Brian Hurst <brian.hurst@gsa.gov>
2017-10-20 15:56:17 -0400

    Remove js height logic for sticky sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,3 @@ import Stickyfill from 'stickyfilljs';
 
 var elements = $('.sticky');
 Stickyfill.add(elements);
-
-var navHeight = function () {
-  var windowHeight = window.innerHeight;
-  var mainContent = $('.usa-layout-docs-main_content')
-  var sideNav = $('.usa-layout-docs-sidenav');
-
-  if (sideNav.height() > windowHeight && mainContent.height() > windowHeight) {
-    sideNav.css('height', (windowHeight));
-  } else {
-    sideNav.css('height', 'auto');
-  }
-};
-
-navHeight();
-
-$( window ).resize(function () {
-  navHeight();
-});
