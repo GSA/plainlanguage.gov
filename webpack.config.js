9bd74317fefa8f1c0f3516262a90e1116dff776a
Nick Lyons <nick.lyons@gsa.gov>
2022-05-09 14:32:57 -0400

    updated webpack config synxtax (#351)

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-es2015']
           }
         }
       }
